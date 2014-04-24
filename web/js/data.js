04556a1082ffb1d8c4a72f32930a44fc06d652fc
Ben Willis <bwillis@manilla.com>
2014-04-23 22:35:30 -0500

    Moving data out of main js file

---
+++ web/js/data.js
@@ -0,0 +1,27 @@
+function getData() {
+  return [
+    {
+      title: "Biggish Data With Rails and Postgresql",
+      room: "BALLROOM1-3",
+      date: "Wed 4/23",
+      time: "11:10am - 11:50am",
+      abstract: "Mature apps face problems with abusive requests like misbehaving users, malicious hackers, and naive scrapers. Too often they drain developer productivity and happiness.\
+                        \
+                        Rack::Attack is middleware to easily throttle abusive requests.\
+                        \
+                        At Kickstarter, we built it to keep our site fast and reliable with little effort. Learn how Rack::Attack works through examples from kickstarter.com. Spend less time dealing with bad apples, and more time on the fun stuff.\
+                        \
+                        Aaron Suggs is the Operations Engineer at Kickstarter, where he backs too many video game projects.\
+                        \
+                        He enjoys writing code that makes developers' lives easier, especially while wearing his grizzly bear coat.", // optional
+      speaker: "Starr Horne",
+      category: "Big Rails"
+    },
+    {
+      title: "Morning Break",
+      room: "Exhibit Hall",
+      date: "Wed 4/23",
+      time: "11:10am - 11:50am"
+    }
+  ];
+}
--- web/js/main.js
+++ web/js/main.js
@@ -1,29 +1,5 @@
-var data = [
-  {
-    title: "Biggish Data With Rails and Postgresql",
-    room: "BALLROOM1-3",
-    date: "Wed 4/23",
-    time: "11:10am - 11:50am",
-    abstract: "Mature apps face problems with abusive requests like misbehaving users, malicious hackers, and naive scrapers. Too often they drain developer productivity and happiness.\
-                        \
-                        Rack::Attack is middleware to easily throttle abusive requests.\
-                        \
-                        At Kickstarter, we built it to keep our site fast and reliable with little effort. Learn how Rack::Attack works through examples from kickstarter.com. Spend less time dealing with bad apples, and more time on the fun stuff.\
-                        \
-                        Aaron Suggs is the Operations Engineer at Kickstarter, where he backs too many video game projects.\
-                        \
-                        He enjoys writing code that makes developers' lives easier, especially while wearing his grizzly bear coat.", // optional
-    speaker: "Starr Horne", // optional
-    category: "Big Rails" // optional
-  },
-  {
-    title: "Morning Break",
-    room: "Exhibit Hall",
-    date: "Wed 4/23",
-    time: "11:10am - 11:50am"
-  }
-];
-var list = $("articles");
-for (var i = 0; i < data.length; i++) {
+window.onload = function () {
 
-}
+  console.log(getData());
+  
+};
