ec31d6cec473764f9707b97d7bafb6546c3a50bb
Cody Schaaf <cschaaf@manilla.com>
2014-04-24 02:25:26 -0500

    Updated the scrapper to include the speakers name for keynotes and added location data for all

--- ruby/scrapper.rb
+++ ruby/scrapper.rb
@@ -30,7 +30,26 @@ class Scrapper
     assign_abstracts! workshop_page, schedules
     assign_abstracts! session_page, schedules
 
-    return schedules, time_slots
+    flat_schedules = get_flattened_schedules schedule_page, schedules
+
+    # return flat_schedules, time_slots
+    flat_schedules
+  end
+
+  def self.get_flattened_schedules(page, schedules)
+    tab_links = page.search('nav#schedule_nav > ul#tabs a[href*="tabs"]')
+    mapper = {}
+    flat_schedules = []
+    tab_links.each { |tab_link|
+      mapper.merge!({ "#{tab_link['href'].gsub(/#/,'')}" => tab_link.text})
+    }
+    schedules.each { |key, value|
+      value.each do |schedule|
+        schedule[:day] = mapper[key]
+        flat_schedules << schedule
+      end
+    }
+    flat_schedules
   end
 
   def self.assign_abstracts! page, schedules
@@ -44,8 +63,7 @@ class Scrapper
       schedules.each do |_,tab|
         corresponding_schedule = tab.find do |schedule|
           next unless schedule[:link_to_talk]
-          anchor_number = schedule[:link_to_talk].match(/\/program#(\w+)/)[1]
-          puts "#{anchor_number} + #{link_anchor}"
+          anchor_number = schedule[:link_to_talk].match(/\/\w+#(\w+)/)[1]
           anchor_number == link_anchor
         end
         break if corresponding_schedule
@@ -58,7 +76,6 @@ class Scrapper
                                          :bio      => bio
                                      })
       end
-      puts corresponding_schedule
     end
   end
 
@@ -66,10 +83,15 @@ class Scrapper
     schedule_page.search('div#tabs_container div[id*="tabs-"]').each do |tab|
       schedules[tab['id']]  = []
       time_slots[tab['id']] = []
+      header = []
       tab.search('table.schedule tr').each_with_index do |row, row_index|
         time_string = row.at('td:eq(1)').text.strip
         time_slots[tab['id']] << time_string
-        row.search('td:not(:eq(1))').each do |column|
+        row.search('td:not(:eq(1))').each_with_index do |column, column_index|
+          if column['class'] == 'header'
+            header << column
+            next
+          end
 
           if (row_span = column['rowspan'])
             time_slot_indexes = []
@@ -82,12 +104,18 @@ class Scrapper
 
           session_type = column['class']
 
-          if (link_to_talk_container = column.at('a[href*="/program"]'))
+          if (link_to_talk_container = (column.at('a[href*="/program"]') || column.at('a[href*="/workshops"]')))
             title = link_to_talk_container.text.strip
             link_to_talk = link_to_talk_container['href']
             speakers = column.at('h6').text.strip
+            location_container =  header[column_index]
+            location = location_container.text.strip if location_container
           elsif session_type == 'general_event'
+            speakers_container = column.at('h6')
             title = column.at('h5').text.strip
+            location_container =  column.at('h4')
+            location = location_container.text.strip if location_container
+            speakers = speakers_container.text.strip if speakers_container
           end
           if time_string.present?
             beginning_time = time_string.gsub(/\s*\-[^\-]+/,'')
@@ -96,6 +124,7 @@ class Scrapper
 
           schedules[tab['id']] << {
             # column:            column,
+            location:          location,
             begining_time:     beginning_time,
             end_time:          end_time,
             link_to_talk:      link_to_talk,
